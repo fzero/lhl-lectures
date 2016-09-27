e96835435e033d33aff025a8e7a5fe4f8db8fc55
Fabio Neves <fabio@fzero.ca>
2016-09-27 11:06:51 -0400

    w4d2 final code for realz

--- w4d2-db-from-js/code/app.js
+++ w4d2-db-from-js/code/app.js
@@ -1,48 +1,17 @@
-const pg = require('pg');
+const musicdb = require('./musicdb') // regular version
+// const musicdb = require('./musicdb-promises') // promises version
 
-// Create a config to configure both pooling behavior
-// and client options.
-// Note: all config is optional and the environment variables
-// will be read if the config is not present
-// You can put this data on a .env file if you'd like.
-const config = {
-  user: 'fzero', //env var: PGUSER
-  database: 'week4', //env var: PGDATABASE
-  password: undefined, //env var: PGPASSWORD
-  port: 5432 //env var: PGPORT
-};
+var artistName = process.argv[2];
+var albumName = process.argv[3];
 
-// Instantiate a new client
-// If there's no config object,
-// the client will read connection information from
-// the same environment variables used by postgres cli tools.
-const client = new pg.Client(config);
-
-// connect to our database
-client.connect((err) => {
-  if (err) throw err;
-
-  client.query('SELECT * FROM artists;', (err, result) => {
-    if (err) throw err;
-
-    // `result` contains metadata about the query and the results themselves.
-    // rowCount = number of results
-    // rows = array of objects containing the actual results
-    console.log(`Number of results: ${result.rowCount}\n`);
-    result.rows.forEach((row) => {
-      let output = [];
-      for (column in row) {
-        output.push(`${column}: ${row[column]}`);
-      }
-      console.log(output.join(', '));
-    })
-
-
-    // Close connection when done
-    client.end((err) => {
-      if (err) throw err;
-    });
-
-  });
-
+// See musicdb.js for function interface
+musicdb.getAlbumTracks(artistName, albumName, (rows) => {
+  console.log(`Found ${rows.length} tracks:\n`);
+  rows.forEach((row) => {
+    let output = [];
+    for (column in row) {
+      output.push(`${column}: ${row[column]}`);
+    }
+    console.log(output.join(', '));
+  })
 });
