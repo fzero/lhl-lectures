f7a726c0c5ae1c5cc42ca00433afd65fe09a2d0b
Fabio Neves <fabio@fzero.ca>
2016-11-02 10:21:10 -0400

    W5D3 post-class code

--- w5d3-websockets/code/server.js
+++ w5d3-websockets/code/server.js
@@ -16,18 +16,25 @@ const app = express()
 // Create the WebSockets server and attach it to express
 const wss = new SocketServer({server: app});
 
+// Stores the current value of the text box on the client
+let currentContents = '';
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  // Echo back messages (testing purposes)
-  ws.on('message', echoBack);
+  // Broadcast current textbox contents on connection
+  wss.broadcast(currentContents);
+
+  // Handle messages
+  ws.on('message', handleMessage);
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
+
 
 // Broadcast - Goes through each client and sends message data
 wss.broadcast = function(data) {
@@ -37,6 +44,15 @@ wss.broadcast = function(data) {
 };
 
 
+// Handles incoming messages.
+// Stores the current state of the textbox and broadcasts it
+function handleMessage(message) {
+  currentContents = message;
+  wss.broadcast(message);
+}
+
+
+// Simply broadcasts the message back to all clients
 function echoBack(message) {
   console.log(`Received: ${message}`)
   wss.broadcast(message);
