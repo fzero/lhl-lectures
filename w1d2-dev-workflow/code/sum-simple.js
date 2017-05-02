91ebf671c84a8e7fd83c22040db556a5e1230892
Fabio Neves <hello+github@fabioneves.ca>
2017-05-02 11:05:28 -0400

    w1d2 Moar comments

--- w1d2-dev-workflow/code/sum-simple.js
+++ w1d2-dev-workflow/code/sum-simple.js
@@ -7,6 +7,8 @@ so that I can know the result
 var numbers = process.argv.slice(2);
 var total = 0;
 
+// We're using a different kind of for loop here.
+// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
 for (var number of numbers) {
   var realNumber = Number(number);
   if (isNaN(realNumber)) {
