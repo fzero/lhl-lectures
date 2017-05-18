b0b89115ff9f4e4a99865373fa38a57a1f44eac4
Fabio Neves <hello+github@fabioneves.ca>
2017-05-18 18:28:27 -0400

    w3d5 Added code

---
+++ w3d5-sass/initial/app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const morgan = require('morgan')
+const sassMiddleware = require('node-sass-middleware')
+
+
+// Initialize express
+const app = express()
+
+// Use EJS for views
+app.set("view engine", "ejs")
+
+// Body parsers
+app.use(bodyParser.urlencoded({extended: false})) // forms
+app.use(bodyParser.json()) // JSON
+
+// SASS preprocessor
+// app.use(sassMiddleware({
+//     src: './stylesheets',
+//     dest: './public/css',
+//     prefix:  '/css'
+// }))
+
+// Serve static content from /public
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Logging
+app.use(morgan('dev'))
+
+
+// Routing goes here
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+// Start the server
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
+})
