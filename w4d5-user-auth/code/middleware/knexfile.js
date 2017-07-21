03946990dbe30a58bbce4e01a6e51daa6f89881e
Fabio Neves <hello+github@fabioneves.ca>
2017-07-21 11:19:57 -0400

    w4d5 Added a version using a custom auth middleware

---
+++ w4d5-user-auth/code/middleware/knexfile.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: false
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations'
+    }
+  }
+};
---
+++ w4d5-user-auth/code/middleware/server.js
@@ -0,0 +1,159 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || 'development';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const sass = require('node-sass-middleware');
+const cookieSession = require('cookie-session');
+const flash = require('connect-flash');
+const knexConfig = require('./knexfile');
+const knex = require('knex')(knexConfig[ENV]);
+
+// Separate file with user-related database logic
+const User = require('./lib/user')(knex);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// Because some dependencies are in devDependencies, they must not
+// be required when the app is running in production environment
+if (ENV === 'development') {
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
+  const knexLogger = require('knex-logger');
+  app.use(knexLogger(knex));
+}
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env.SESSION_SECRET || 'development']
+}));
+
+app.use(flash());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/styles', sass({
+  src: __dirname + '/styles',
+  dest: __dirname + '/public/styles',
+  debug: true,
+  outputStyle: 'expanded'
+}));
+
+app.use(express.static('public'));
+
+// Aunthetication middleware
+// See http://expressjs.com/en/guide/writing-middleware.html
+function authMiddleware(req, res, next) {
+  // No session? No user
+  if (!req.session.user_id) {
+    req.user = null // Making sure user is reset when logged out
+    next()          // Always call next()
+    return
+  }
+  // If the user is logged in, add user object to request
+  User.find(req.session.user_id) // Get user info from DB
+  .then((user) => {              // Success!
+    req.user = user              // Set user on request
+    next()
+  })
+  .catch(() => {                 // User not found!
+    req.user = null              // Reset user
+    next()
+  })
+}
+
+app.use(authMiddleware) // Activate middleware
+
+
+/*
+ * ROUTES
+ */
+
+app.get('/', (req, res) => {
+  if (req.user) { // Created by our middleware!
+    res.render('app', {
+      errors: req.flash('errors'),
+      info: req.flash('info'),
+      user: req.user
+    })
+  }
+  else {
+    // If the user is not logged in, render the login/register page
+    res.render('index', {
+      errors: req.flash('errors'),
+      info: req.flash('info')
+    })
+  }
+})
+
+
+// Update profile
+app.post('/profile', (req, res) => {
+  // We'll update what we need to. More details in lib/user.js
+  User.update(req.session.user_id, req.body.email, req.body.password)
+  .then(() => {
+    // Indicate to the user that the update was successful
+    req.flash('info', 'updated your profile');
+    res.redirect('/');
+  }).catch((err) => {
+    // In the even that an error occurred at any point during the promise
+    // chain, add the error message to the flash errors and redirect.
+    req.flash('errors', err.message);
+    res.redirect('/');
+  });
+});
+
+
+app.get('/logout', (req, res) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+
+app.post('/login', (req, res) => {
+  User.authenticate(req.body.email, req.body.password)
+  .then((user) => {
+    // If email and password match, we assign the id to the session
+    req.session.user_id = user.id;
+    res.redirect('/');
+  }).catch((err) => {
+    // In the even that an error occurred at any point during the promise
+    // chain, add the error message to the flash errors and redirect.
+    req.flash('errors', err.message);
+    res.redirect('/');
+  });
+});
+
+
+app.post('/register', (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    // If the registration form was submitted without a value for email or
+    // password, then set an error message and redirect.
+    req.flash('errors', 'email and password are required');
+    res.redirect('/');
+    // IMPORTANT: always return after sending a response, whether it's a
+    // redirect, render, send, end, json, or whatever.
+    return;
+  }
+  User.add(req.body.email, req.body.password)
+  .then(() => {
+    // This callback will be called after the promise returned by the last
+    // call to .then has resolved. That happens after the user is inserted
+    // into the database.
+    req.flash('info', 'account successfully created');
+    res.redirect('/');
+  }).catch((err) => {
+    // In the even that an error occurred at any point during the promise
+    // chain, add the error message to the flash errors and redirect.
+    req.flash('errors', err.message);
+    res.redirect('/');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Example app listening on port ' + PORT);
+});
