58c2c6f66e7a80f7828b23117e54f9fe604222a4
Fabio Neves <hello+github@fabioneves.ca>
2017-12-08 10:40:38 -0500

    w6d5 added if inversion info

--- w6d5-nested-routes/code/app/controllers/bicycles_controller.rb
+++ w6d5-nested-routes/code/app/controllers/bicycles_controller.rb
@@ -6,7 +6,9 @@ class BicyclesController < ApplicationController
     @bicycles = @style.bicycles if @style.present?
 
     # Add filtering based on form fields (when present)
-    @bicycles = @bicycles.where(model: params[:model]) if params[:model].present?
+    if params[:model].present? # long `if` format, works exactly the same as below
+      @bicycles = @bicycles.where(model: params[:model])
+    end
     @bicycles = @bicycles.where(style_id: params[:style]) if params[:style].present?
     @bicycles = @bicycles.where(brand_id: params[:brand]) if params[:brand].present?
     @bicycles = @bicycles.where(speeds: params[:speeds]) if params[:speeds].present?
