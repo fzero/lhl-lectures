970d3ebcc58c25591add0235d92bb76fa38ad8d7
Fabio Neves <hello+github@fabioneves.ca>
2018-08-29 11:02:12 -0400

    ADV organizing actions better

--- ADV-redux-intro/todos/src/actions/index.js
+++ ADV-redux-intro/todos/src/actions/index.js
@@ -19,14 +19,14 @@ export const addTodo = text => ({
   text
 })
 
-export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
-})
-
 export const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
   id
+})
+
+export const setVisibilityFilter = filter => ({
+  type: 'SET_VISIBILITY_FILTER',
+  filter
 })
 
 /*
